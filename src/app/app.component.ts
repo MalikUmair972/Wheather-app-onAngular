2b05dddd98825fc28d1d35d62ee6844400f090b3
MalikUmair972 <mu501362@gmail.com>
2024-12-06 11:54:14 +0500

    Display Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ApiServiceService } from './api-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Weather-app';
+  weatherData: any;
+  country: string = '';
+  inputValue: string = ''; 
+
+  constructor(private weatherService: ApiServiceService) {}
+
+  searchWeather() {
+    this.country = this.inputValue
+    if (!this.country.trim()) {
+      return;
+    }
+
+    this.weatherService.getWeatherByCountry(this.country).subscribe(
+      (data: any) => {
+        this.weatherData = data;
+      },
+      (error) => {
+        alert('Error fetching weather data. Please try again.');
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
